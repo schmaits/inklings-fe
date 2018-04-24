1b4c5899bbc85f55b00022ca556bae40c5a9b566
schmaits <stansfield.c.r@gmail.com>
2018-04-24 15:13:55 +0100

    Takes out call for book data for currently reading

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -5,9 +5,13 @@ import { getCall } from '../lib/api';
 
 class Homepage extends Component {
     state = {
-        user: {},
+        user: {
+            currentlyReading: [],
+            toRead: [],
+            read: []
+        },
         quote: {},
-        currentlyReading: []
+        clubs: []
     }
 
     componentDidMount () {
@@ -34,14 +38,6 @@ class Homepage extends Component {
                     quote: updatedQuote
                 })
             })
-            .then(() => {
-                return getCall(`/books/${this.state.user.currentlyReading}`)
-            })
-            .then(currentlyReading => {
-                this.setState({
-                    currentlyReading: currentlyReading.book
-                })
-            })
             .catch(err => {
                 console.log(err)
             })
@@ -59,24 +55,19 @@ class Homepage extends Component {
                     <div className="column">
                         <p className="title">Hello {this.state.user.firstName}!</p>
                         <p>{this.state.quote.body}</p>
-                        <p className="has-text-left">- {this.state.quote.bookTitle}</p>
+                        <p className="has-text-right">- {this.state.quote.bookTitle}</p>
                     </div>
                 </div>
 
-                <div className="section">
+                <div>
                     <div className="tile is-ancestor">
                         <div className="tile is-parent is-3">
                             <div className="tile is-child">
-                                <p className="is-size-4">Currently reading</p>
-                                {this.state.currentlyReading.map(book => {
-                                    const { author, coverImageUrl, genres, rating, title, _id } = book; 
+                                <p className="is-size-5">Currently reading</p>
+                                {this.state.user.currentlyReading.map(book => {
                                     return <BookPreview 
-                                        key={_id}
-                                        title={title} 
-                                        author={author}
-                                        coverImageUrl={coverImageUrl}
-                                        genres={genres}
-                                        rating={rating}
+                                        key={book}
+                                        bookId={book}
                                     />
                                 })}
                             </div>
@@ -84,84 +75,14 @@ class Homepage extends Component {
                         </div>
                         <div className="tile is-parent is-10 is-vertical">
                             <div className="tile is-child">
-                                <p>Clubs</p>
-                                <div className="level">
-                                <div className="card">
-                                <div className="card-image">
-                                    <figure className="image is-4by3">
-                                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder"/>
-                                    </figure>
-                                </div>
-                                <div className="card-content">
-                                    <div className="media">
-                                    <div className="media-left">
-                                        <figure className="image is-48x48">
-                                            <p>Rating</p>
-                                        </figure>
-                                    </div>
-                                    <div className="media-content">
-                                        <p className="title is-4">Book 1</p>
-                                    </div>
-                                    </div>
-
-                                    <div className="content">
-                                        Book 1 info
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="card">
-                                <div className="card-image">
-                                    <figure className="image is-4by3">
-                                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder"/>
-                                    </figure>
-                                </div>
-                                <div className="card-content">
-                                    <div className="media">
-                                    <div className="media-left">
-                                        <figure className="image is-48x48">
-                                            <p>Rating</p>
-                                        </figure>
-                                    </div>
-                                    <div className="media-content">
-                                        <p className="title is-4">Book 1</p>
-                                    </div>
-                                    </div>
-
-                                    <div className="content">
-                                        Book 1 info
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="card">
-                                <div className="card-image">
-                                    <figure className="image is-4by3">
-                                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder"/>
-                                    </figure>
-                                </div>
-                                <div className="card-content">
-                                    <div className="media">
-                                    <div className="media-left">
-                                        <figure className="image is-48x48">
-                                            <p>Rating</p>
-                                        </figure>
-                                    </div>
-                                    <div className="media-content">
-                                        <p className="title is-4">Book 1</p>
-                                    </div>
-                                    </div>
-
-                                    <div className="content">
-                                        Book 1 info
-                                    </div>
-                                </div>
-                            </div>
-                                </div>
+                                <p className="is-size-5">Clubs</p>
+                                
                             </div>
                             <div className="tile is-child">
-                                <p>To Read</p>
+                                <p className="is-size-5">To Read</p>
                             </div>
                             <div className="tile is-child">
-                                <p>Read</p>
+                                <p className="is-size-5">Read</p>
                             </div>
                         </div>
                     </div>
