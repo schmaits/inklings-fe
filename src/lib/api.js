2dfffa3ad7d6a0d48404b931518d6c5605f1e5b8
schmaits <stansfield.c.r@gmail.com>
2018-04-24 15:41:04 +0100

    Creates put request function

--- src/lib/api.js
+++ src/lib/api.js
@@ -5,3 +5,14 @@ export function getCall (endpoint) {
         return res.json()
     })
 }
+
+export function putCall (endpoint, newData) {
+    return fetch(`http://localhost:3010/api${endpoint}`, {
+        method: 'PUT',
+        headers: new Headers({
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }),
+        body: JSON.stringify(newData)
+    })
+}
