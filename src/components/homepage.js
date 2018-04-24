10cfb2f6d2f2f74e3e311c5dd7cede98cf30825d
schmaits <stansfield.c.r@gmail.com>
2018-04-24 13:11:26 +0100

    Amends layout

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -13,7 +13,146 @@ class Homepage extends Component {
                         <p>Quote</p>
                     </div>
                 </div>
-                <h1>Currently reading...</h1>
+            
+                <div className="tile is-ancestor">
+                    <div className="tile is-parent is-2">
+                        <div className="tile is-child">
+                            <p>Currently reading</p>
+                            <div className="card">
+                            <div className="card-image">
+                                <figure className="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                                </figure>
+                            </div>
+                            <div className="card-content">
+                                <div className="media">
+                                <div className="media-left">
+                                    <figure className="image is-48x48">
+                                        <p>Rating</p>
+                                    </figure>
+                                </div>
+                                <div className="media-content">
+                                    <p className="title is-4">Book 1</p>
+                                </div>
+                                </div>
+
+                                <div className="content">
+                                    Book 1 info
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="card-image">
+                                <figure className="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                                </figure>
+                            </div>
+                            <div className="card-content">
+                                <div className="media">
+                                <div className="media-left">
+                                    <figure className="image is-48x48">
+                                        <p>Rating</p>
+                                    </figure>
+                                </div>
+                                <div className="media-content">
+                                    <p className="title is-4">Book 1</p>
+                                </div>
+                                </div>
+
+                                <div className="content">
+                                    Book 1 info
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+
+                    </div>
+                    <div className="tile is-parent is-10 is-vertical">
+                        <div className="tile is-child">
+                            <p>Clubs</p>
+                            <div className="level">
+                            <div className="card">
+                            <div className="card-image">
+                                <figure className="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                                </figure>
+                            </div>
+                            <div className="card-content">
+                                <div className="media">
+                                <div className="media-left">
+                                    <figure className="image is-48x48">
+                                        <p>Rating</p>
+                                    </figure>
+                                </div>
+                                <div className="media-content">
+                                    <p className="title is-4">Book 1</p>
+                                </div>
+                                </div>
+
+                                <div className="content">
+                                    Book 1 info
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="card-image">
+                                <figure className="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                                </figure>
+                            </div>
+                            <div className="card-content">
+                                <div className="media">
+                                <div className="media-left">
+                                    <figure className="image is-48x48">
+                                        <p>Rating</p>
+                                    </figure>
+                                </div>
+                                <div className="media-content">
+                                    <p className="title is-4">Book 1</p>
+                                </div>
+                                </div>
+
+                                <div className="content">
+                                    Book 1 info
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="card-image">
+                                <figure className="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                                </figure>
+                            </div>
+                            <div className="card-content">
+                                <div className="media">
+                                <div className="media-left">
+                                    <figure className="image is-48x48">
+                                        <p>Rating</p>
+                                    </figure>
+                                </div>
+                                <div className="media-content">
+                                    <p className="title is-4">Book 1</p>
+                                </div>
+                                </div>
+
+                                <div className="content">
+                                    Book 1 info
+                                </div>
+                            </div>
+                        </div>
+                            </div>
+                        </div>
+                        <div className="tile is-child">
+                            <p>To Read</p>
+                        </div>
+                        <div className="tile is-child">
+                            <p>Read</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                {/* <h1>Currently reading...</h1>
                 <div className="section columns">
                     <div className="column">
                         <div className="card">
@@ -168,7 +307,7 @@ class Homepage extends Component {
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </div>
         )
     }
