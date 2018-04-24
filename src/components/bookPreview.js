4979f94959350b9a1dcb5318656808271e217270
schmaits <stansfield.c.r@gmail.com>
2018-04-24 15:12:37 +0100

    Changes to React component

--- src/components/bookPreview.js
+++ src/components/bookPreview.js
@@ -1,33 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { getCall } from '../lib/api';
 
-const BookPreview = (props) => {
-    const { author, coverImageUrl, genres, rating, title } = props;
-    return (
-        <div className="card">
-            <div className="card-image">
-                <figure className="image">
-                    <img src={coverImageUrl} alt={title}/>
-                </figure>
-            </div>
-            <div className="card-content">
-                <div className="media">
-                    {/* <div className="media-left">
-                        <figure className="image is-48x48">
-                            <p>Rating</p>
-                        </figure>
-                    </div> */}
-                    <div className="media-content">
-                        <p className="title is-4">{title}</p>
+class BookPreview extends Component {
+    state = {
+        book: {}
+    }
+
+    componentDidMount () {
+        getCall(`/books/${this.props.bookId}`)
+            .then(bookData => {
+                this.setState({
+                    book: bookData.book[0]
+                })
+            })
+    }
+
+    render () {
+        const { author, coverImageUrl, genres, rating, title } = this.state.book;
+        return (
+            <div className="card">
+                <div className="card-image">
+                    <figure className="image">
+                        <img src={coverImageUrl} alt={title}/>
+                    </figure>
+                </div>
+                <div className="card-content">
+                    <div className="media">
+                        <div className="media-content">
+                            <p className="title is-4">{title}</p>
+                        </div>
+                    </div>
+                    <div className="content">
+                        <p>{author}</p>
+                        <p>{rating}/5</p>
+                        <p>{genres}</p>
                     </div>
                 </div>
-                <div className="content">
-                    <p>{author}</p>
-                    <p>{rating}/5</p>
-                    <p>{genres}</p>
-                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default BookPreview;
