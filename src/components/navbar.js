383572e93d226599ff1ab2814ab4ab3ca4d567c9
schmaits <stansfield.c.r@gmail.com>
2018-04-24 12:14:44 +0100

    Makes transparent

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div className="navbar">
+        <div className="navbar is-transparent">
             <div className="navbar-brand">
-                <div className="navbar-item"><p>TITLE PENDING</p></div>
+                <div className="navbar-item has-text-light"><p>TITLE PENDING</p></div>
             </div>
             <div className="navbar-menu">
                 <div className="navbar-start">
-                    <div className="navbar-item"><p>Your profile</p></div>
+                    <div className="navbar-item has-text-light"><p>Your profile</p></div>
                 </div>
                 <div className="navbar-end">
-                    <div className="navbar-item"><p>Item 1</p></div>
-                    <div className="navbar-item"><p>Item 2</p></div>
-                    <div className="navbar-item"><p>Item 3</p></div>
+                    <div className="navbar-item has-text-light"><p>Item 1</p></div>
+                    <div className="navbar-item has-text-light"><p>Item 2</p></div>
+                    <div className="navbar-item has-text-light"><p>Item 3</p></div>
                 </div>
             </div>
         </div>
