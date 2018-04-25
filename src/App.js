ee73806f5ac4190860c46d29d7796da34ac53789
schmaits <stansfield.c.r@gmail.com>
2018-04-25 15:21:21 +0100

    Creates basic routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Hero from './components/hero';
 import Homepage from './components/homepage';
+import BookView from './components/bookView';
+import ClubView from './components/clubView';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Hero/>
-        </header>
-          <Homepage/>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+            <div>
+              <Hero/>
+            </div>
+            <div>
+              <Switch>
+                <Route exact path="/" component={Homepage}/>
+                <Route path="/books/:bookId" component={BookView}/>
+                <Route path="/clubs/:clubId" component={ClubView}/>
+              </Switch>
+            </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
