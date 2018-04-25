41fdfd96d3ed2e66fedc91a684a332faeebc2c11
schmaits <stansfield.c.r@gmail.com>
2018-04-25 16:15:28 +0100

    Fetches book data, displaying image and rating

--- src/components/bookView.js
+++ src/components/bookView.js
@@ -3,11 +3,28 @@ import React, { Component } from 'react';
 import { getCall } from '../lib/api';
 
 class BookView extends Component {
+    state = {
+        book: {}
+    }
+
+    componentDidMount () {
+        getCall(`/books/${this.props.match.params.bookId}`)
+            .then(bookData => {
+                this.setState({
+                    book: bookData.book[0]
+                })
+            })
+    }
+
     render () {
+        console.log(this.state)
         return (
             <div className="tile is-ancestor box">
-                <div className="tile is-parent is-vertical box">
-                    <div className="tile is-child box">Picture, rating</div>
+                <div className="tile is-parent is-vertical box is-3">
+                    <div className="tile is-child box">
+                        <img src={this.state.book.coverImageUrl}/>
+                        <p>{this.state.book.rating}/5</p>
+                    </div>
                     <div className="tile is-child box">Clubs reading</div>
                 </div>
                 <div className="tile is-parent is-vertical box">
