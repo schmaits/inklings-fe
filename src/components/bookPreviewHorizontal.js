4154470237e2374017919389664e519f312c20cd
schmaits <stansfield.c.r@gmail.com>
2018-04-25 14:28:19 +0100

    Creates component and fetches datd

---
+++ src/components/bookPreviewHorizontal.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import { getCall } from '../lib/api';
+
+class BookPreviewHorizontal extends Component {
+    state = {
+        book: {}
+    }
+
+    componentDidMount () {
+        getCall(`/books/${this.props.bookId}`)
+            .then(bookData => {
+                this.setState({
+                    book: bookData.book[0]
+                })
+            })
+    }
+
+    render () {
+        return (
+            <article className="media">
+                <figure className="media-left">
+                    <p className="image is-96x96">
+                    <img src={this.state.book.coverImageUrl} alt={this.state.book.title}/>
+                    </p>
+                </figure>
+                <div className="media-content">
+                    <div className="content">
+                    <p>
+                        <strong>{this.state.book.title}</strong>
+                        <br/>
+                        {this.state.book.author}
+                    </p>
+                    </div>
+                </div>
+            </article>
+        )
+    }
+}
+
+export default BookPreviewHorizontal;
+
