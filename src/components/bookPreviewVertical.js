2aa8f273e3282dd426fb7069f30009415d398263
schmaits <stansfield.c.r@gmail.com>
2018-04-25 15:30:29 +0100

    Notrequired for now

--- src/components/bookPreviewVertical.js
+++ src/components/bookPreviewVertical.js
@@ -26,28 +26,32 @@ class BookPreviewVertical extends Component {
     render () {
         const { author, coverImageUrl, genres, rating, title } = this.state.book;
         return (
-            <div className="card">
-                <div className="card-image level">
-                    <figure className="image is-96x96">
-                        <img src={coverImageUrl} alt={title}/>
-                    </figure>
-                </div>
-                <div className="card-content">
-                    <div className="media">
-                        <div className="media-content">
-                            <p className="title is-4">{title}</p>
-                        </div>
-                    </div>
-                    <div className="content">
-                        <p>{author}</p>
-                        <p>{rating}/5</p>
-                        <p>{genres}</p>
-                        <a className="button" onClick={this.moveToRead} >Finished</a>
-                    </div>
-                </div>
-            </div>
+           <div className="">
+           </div>
         )
     }
 }
 
 export default BookPreviewVertical;
+
+
+{/* <div className="card">
+<div className="card-image level">
+    <figure className="image is-96x96">
+        <img src={coverImageUrl} alt={title}/>
+    </figure>
+</div>
+<div className="card-content">
+    <div className="media">
+        <div className="media-content">
+            <p className="title is-4">{title}</p>
+        </div>
+    </div>
+    <div className="content">
+        <p>{author}</p>
+        <p>{rating}/5</p>
+        <p>{genres}</p>
+        <a className="button" onClick={this.moveToRead} >Finished</a>
+    </div>
+</div>
+</div> */}
