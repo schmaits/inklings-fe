5015fa5c1d190d9a65162d65cf5b9bd8701d2ca1
schmaits <stansfield.c.r@gmail.com>
2018-04-25 13:35:11 +0100

    Fixes styling

--- src/components/clubPreview.js
+++ src/components/clubPreview.js
@@ -22,15 +22,6 @@ class ClubPreview extends Component {
     render () {
         const { name } = this.props;
         return (
-            // <div className="columns">
-            //     <div className="column">
-            //         <img src={this.state.currentlyReading.coverImageUrl} alt={this.state.currentlyReading.title}/>
-            //     </div>
-            //     <div className="column">
-            //         <p className="title">{name}</p>
-            //         <p>Currently reading: {this.state.currentlyReading.title}</p>
-            //     </div>
-            // </div>
             <article className="media">
                 <figure className="media-left">
                     <p className="image is-96x96">
@@ -54,34 +45,3 @@ class ClubPreview extends Component {
 }
 
 export default ClubPreview;
-
-
-{/* <article className="media">
-  <figure className="media-left">
-    <p className="image is-64x64">
-      <img src={this.state.currentlyReading.coverImageUrl}/>
-    </p>
-  </figure>
-  <div className="media-content">
-    <div className="content">
-      <p>
-        <strong>{name}</strong>
-        <br/>
-        Currently reading: {this.state.currentlyReading.title}
-      </p>
-    </div>
-    <nav class="level is-mobile">
-      <div class="level-left">
-        <a class="level-item">
-          <span class="icon is-small"><i class="fas fa-reply"></i></span>
-        </a>
-        <a class="level-item">
-          <span class="icon is-small"><i class="fas fa-retweet"></i></span>
-        </a>
-        <a class="level-item">
-          <span class="icon is-small"><i class="fas fa-heart"></i></span>
-        </a>
-      </div>
-    </nav>
-  </div>
-</article> */}
