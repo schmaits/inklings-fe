b20a40083b626b0ff63fe8b0dcebffcee95d021e
schmaits <stansfield.c.r@gmail.com>
2018-04-25 14:49:01 +0100

    Fixes error handling in promise

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,9 +1,11 @@
 export function getCall (endpoint) {
     return fetch(`http://localhost:3010/api${endpoint}`)
-    .then(res => {
-        if (res.status === 500) return Promise.reject(new Error('Not found'))
-        return res.json()
-    })
+        .then(res => {
+            return res.json()
+        })
+        .catch(err => {
+            console.log(err)
+        });
 }
 
 export function putCall (endpoint, newData) {
