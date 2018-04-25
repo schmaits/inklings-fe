b0902cb5d38bf463acc25e6daca312e05aa8b3a0
schmaits <stansfield.c.r@gmail.com>
2018-04-25 16:28:11 +0100

    Fetches quote, then picks a randmon one to display

--- src/components/bookView.js
+++ src/components/bookView.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import faker from 'faker';
 
 import { getCall } from '../lib/api';
 
 class BookView extends Component {
     state = {
-        book: {}
+        book: {},
+        quotes: []
     }
 
     componentDidMount () {
@@ -14,10 +16,27 @@ class BookView extends Component {
                     book: bookData.book[0]
                 })
             })
+            .then(() => {
+                return getCall('/quotes')
+            })
+            .then(quotesData => {
+                const bookQuotes = quotesData.allQuotes.filter(quote => {
+                    return quote.book === this.state.book._id
+                })
+                return bookQuotes;
+            })
+            .then(bookQuotes => {
+                this.setState({
+                    quotes: bookQuotes
+                })
+            })
+            .catch(err => {
+                if (err) console.log(err);
+            });
     }
 
     render () {
-        console.log(this.state)
+        console.log(this.state.quote)
         return (
             <div className="tile is-ancestor box">
                 <div className="tile is-parent is-vertical box is-3">
@@ -28,7 +47,10 @@ class BookView extends Component {
                     <div className="tile is-child box">Clubs reading</div>
                 </div>
                 <div className="tile is-parent is-vertical box">
-                    <div className="tile is-child box">Quotes from book</div>
+                    <div className="tile is-child box has-text-centered">
+                        { this.state.quotes.length === 0 ? (<p>There are no quotes for this book yet!</p>) : (
+                            <p>"{faker.random.arrayElement(this.state.quotes).body}"</p>) }
+                    </div>
                     <div className="tile is-child box">Recent comments</div>
                 </div>
             </div>
