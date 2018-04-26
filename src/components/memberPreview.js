48854266884bbe866dc36e90ce2ed17f1610393a
schmaits <stansfield.c.r@gmail.com>
2018-04-26 13:12:55 +0100

    Creates member preview component

---
+++ src/components/memberPreview.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+
+import { getCall } from '../lib/api';
+
+class MemberPreview extends Component {
+    state = {
+        user: {}
+    }
+
+    componentDidMount () {
+        getCall(`/users/${this.props.userId}`)
+            .then(userData => {
+                this.setState({
+                    user: userData.user[0]
+                })
+            })
+    }
+
+    render () {
+        console.log(this.props.admin)
+        return (
+            <article className="media">
+                <figure className="media-left">
+                    <p className="image is-48x48">
+                    <img src={this.state.user.profilePictureUrl} alt={`${this.state.user.firstName} ${this.state.user.secondName}`}/>
+                    </p>
+                </figure>
+                <div className="media-content">
+                    <div className="content">
+                  {this.props.admin ? (<span className="icon has-text-warning">
+                        <i className="fa fa-star"/>
+                  </span>) : null}
+                        <strong className="has-text-black">{this.state.user.username}</strong>
+                        <br/>
+                        <p>{this.state.user.firstName} {this.state.user.secondName}</p>
+                        {/* <p>{this.state.book.rating}/5</p>
+                        <p>{this.state.book.genres}</p> */}
+                    </div>
+                </div>
+            </article>
+        )
+    }
+}
+
+export default MemberPreview;
+
