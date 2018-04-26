27cfa30ab3ca942d7e4ab021a8a19be26aa342cd
schmaits <stansfield.c.r@gmail.com>
2018-04-26 13:13:20 +0100

    Passes down member information to member preview

--- src/components/clubView.js
+++ src/components/clubView.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import faker from 'faker';
 
 import { getCall } from '../lib/api';
+import MemberPreview from './memberPreview';
 
 class ClubView extends Component {
     state = {
-        club: {}
+        club: {
+            members: []
+        },
+        currentBook: {},
+        quotes: []
     }
 
     componentDidMount () {
@@ -14,21 +20,68 @@ class ClubView extends Component {
                     club: clubData.club
                 });
             })
+            .then(() => {
+                return getCall(`/books/${this.state.club.currentlyReading}`)
+            })
+            .then(bookData => {
+                this.setState({
+                    currentBook: bookData.book[0]
+                })
+            })
+            .then(() => {
+                return getCall('/quotes')
+            })
+            .then(quotesData => {
+                const bookQuotes = quotesData.allQuotes.filter(quote => {
+                    return quote.book === this.state.currentBook._id
+                })
+                return bookQuotes;
+            })
+            .then(bookQuotes => {
+                this.setState({
+                    quotes: bookQuotes
+                })
+            })
             .catch(err => {
                 console.log(err);
             })
     }
 
     render () {
-        console.log(this.state.club)
+        const { coverImageUrl, title, author, rating } = this.state.currentBook;
         return (
             <div className="tile is-ancestor box">
-                <div className="tile is-parent is-vertical box">
-                    <div className="tile is-child box">Currently reading, picture, rating</div>
-                    <div className="tile is-child box">Member list, member preview card</div>
+                <div className="tile is-parent is-vertical is-3 box">
+                    <div className="tile is-child box">
+                        <p>Currently Reading</p>
+                        <img src={coverImageUrl} alt={title}/>
+                        <p>{title}</p>
+                        <p>{author}</p>
+                        <p>{rating}/5</p>
+                    </div>
+                    <div className="tile is-child box">
+                        <p>Members</p>
+                        {this.state.club.admin ? 
+                            (<MemberPreview 
+                                userId={this.state.club.admin}
+                                admin={true}
+                            />) : (<p>There are no members</p>)
+                        }
+                        {this.state.club.members.map(member => {
+                            return <MemberPreview
+                                userId={member}
+                                admin={false}
+                            />
+                        })}
+                    </div>
                 </div>
                 <div className="tile is-parent is-vertical box">
-                    <div className="tile is-child box">Quotes from current book</div>
+                    <div className="tile is-child box has-text-centered">
+                        { this.state.quotes.length === 0 ? 
+                            (<p className="has-text-centered">There are no quotes for this book yet!</p>) : 
+                            (<p>"{faker.random.arrayElement(this.state.quotes).body}"</p>) 
+                        }
+                    </div>
                     <div className="tile is-child box">Discussion board</div>
                 </div>
             </div>
