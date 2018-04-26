672ae493250542f4818d91d715ad8d2257185f65
schmaits <stansfield.c.r@gmail.com>
2018-04-26 13:13:42 +0100

    Brings in font-aesome

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "bulma/css/bulma.css";
+import 'bulma/css/bulma.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ 
