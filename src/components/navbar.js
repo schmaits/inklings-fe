07efc62f334762341fa4c599baf2c6eab1333bfe
schmaits <stansfield.c.r@gmail.com>
2018-04-28 13:09:45 +0100

    Adds home tab

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,9 +5,11 @@ const Navbar = () => {
     return (
         <div className="navbar is-transparent">
             <div className="navbar-brand">
-                <div className="navbar-item has-text-light"><Link className="has-text-light" to="/">TITLE PENDING</Link></div>
             </div>
             <div className="navbar-menu">
+                <div className="navbar-start">
+                    <div className="navbar-item has-text-light"><Link className="has-text-light" to="/">Home</Link></div>
+                </div>
                 <div className="navbar-end">
                     <div className="navbar-item has-text-light"><p>Item 1</p></div>
                     <div className="navbar-item has-text-light"><p>Item 2</p></div>
