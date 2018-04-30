68f0646e11b8df2c0bda9cf9b518f9d6f969b066
schmaits <stansfield.c.r@gmail.com>
2018-04-30 16:49:28 +0100

    Fetches book data and displays it

--- src/components/bookBrowse.js
+++ src/components/bookBrowse.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 
+import { getCall } from '../lib/api';
+
+import BookPreview from './bookPreview';
+
 class BookBrowse extends Component {
+    state = {
+        books: []
+    }
+
+    componentDidMount () {
+        getCall('/books')
+            .then(bookData => {
+                console.log(bookData.allBooks[0])
+                this.setState({
+                    books: bookData.allBooks
+                })
+            })
+    }
+
     render () {
         return (
             <div>
-                BOOKS BOOKS BOOKS 
+                {this.state.books.map(book => {
+                    return <div key={book._id} className="tile box">
+                        <BookPreview 
+                            bookId={book._id}
+                        />
+                    </div>
+                })}
             </div>
         )
     }
