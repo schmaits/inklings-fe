6e8ab8fa6c24647645530400d574db58b4f08498
schmaits <stansfield.c.r@gmail.com>
2018-04-30 10:20:15 +0100

    Adds styling

--- src/components/notFound.js
+++ src/components/notFound.js
@@ -6,6 +6,9 @@ import '../App.css';
 const NotFound = () => {
     return (
         <div>
+            <br/>
+            <br/>
+            <br/>
             <p className="has-text-centered has-text-light title">That page doesn't exist</p>
             <p className="has-text-centered"><Link to="/" className="has-text-light title">Would you like to go back home?</Link></p>
             <p className="not-found">404</p>
