a3b7e0473c4cce2f30ecc37243cef444b6203452
schmaits <stansfield.c.r@gmail.com>
2018-05-01 11:27:51 +0100

    On click stars light up with your vote

--- src/components/ratingGraphic.js
+++ src/components/ratingGraphic.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 class RatingGraphic extends Component {
     state = {
-        tempRating: this.props.aveRating
+        tempRating: this.props.aveRating,
+        vote: undefined
     }
 
     componentWillReceiveProps(nextProps) {
@@ -12,14 +13,25 @@ class RatingGraphic extends Component {
     }
 
     onHover = (event) => {
+        this.state.vote ? null :
         this.setState({
             tempRating: event.target.id
         })
     }
 
     onMouseOff = (event) => {
+        this.state.vote ? null :
         this.setState({
             tempRating: this.props.aveRating
+        })
+    }
+
+    vote = (event) => {
+        event.preventDefault();
+
+        this.setState({
+            vote: event.target.id,
+            tempRating: event.target.id
         })
     }
     
@@ -29,11 +41,11 @@ class RatingGraphic extends Component {
         return (
             <div>
                 <span className="icon has-size-5">
-                    <i id="1" onMouseOut={this.onMouseOff} onMouseOver={this.onHover} className={ this.state.tempRating >= 1 ? yellowStar : hollowStar}/>
-                    <i id="2" onMouseOut={this.onMouseOff} onMouseOver={this.onHover} className={ this.state.tempRating >= 2 ? yellowStar : hollowStar}/>
-                    <i id="3" onMouseOut={this.onMouseOff} onMouseOver={this.onHover} className={ this.state.tempRating >= 3 ? yellowStar : hollowStar}/>
-                    <i id="4" onMouseOut={this.onMouseOff} onMouseOver={this.onHover} className={ this.state.tempRating >= 4 ? yellowStar : hollowStar}/>
-                    <i id="5" onMouseOut={this.onMouseOff} onMouseOver={this.onHover} className={ this.state.tempRating >= 5 ? yellowStar : hollowStar}/>
+                    <i id="1" onClick={this.vote} onMouseOut={this.onMouseOff} onMouseOver={this.onHover} className={ this.state.tempRating >= 1 ? yellowStar : hollowStar}/>
+                    <i id="2" onClick={this.vote} onMouseOut={this.onMouseOff} onMouseOver={this.onHover} className={ this.state.tempRating >= 2 ? yellowStar : hollowStar}/>
+                    <i id="3" onClick={this.vote} onMouseOut={this.onMouseOff} onMouseOver={this.onHover} className={ this.state.tempRating >= 3 ? yellowStar : hollowStar}/>
+                    <i id="4" onClick={this.vote} onMouseOut={this.onMouseOff} onMouseOver={this.onHover} className={ this.state.tempRating >= 4 ? yellowStar : hollowStar}/>
+                    <i id="5" onClick={this.vote} onMouseOut={this.onMouseOff} onMouseOver={this.onHover} className={ this.state.tempRating >= 5 ? yellowStar : hollowStar}/>
                 </span>
             </div>
         )
